009ad36a90f3a0f8261e30c38ad7fd23588cc661
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-10-19 07:54:02 -0500

    Basic functionality finished

--- master.js
+++ master.js
@@ -1,4 +1,7 @@
-var WIDTH, HEIGHT;
+let n = 20,
+    particles = [],
+    cutoff = 100,
+    WIDTH, HEIGHT;
 
 var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d');
@@ -17,17 +20,50 @@ function init() {
 
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
     ctx.beginPath();
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = '#fefefe';
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
     ctx.closePath();
+
+    ctx.strokeStyle = "grey";
+
+    for (let i = 0; i < n; i++) {
+        const x = random(WIDTH),
+            y = random(HEIGHT);
+
+        const pos = new Vector(x, y);
+        const vel = Vector.random();
+
+        particles.push(new Particle(pos, vel, "black"))
+    }
 
     ani();
 }
 
 function ani() {
+    ctx.beginPath();
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    ctx.closePath();
 
+    for (let i = 0; i < particles.length; i++) {
+        let p = particles[i];
 
-    //requestAnimationFrame(ani);
+        p.borders(1);
+        p.update();
+
+        for (let j = 0; j < particles.length; j++) {
+            let p2 = particles[j];
+
+            if (Vector.dist(p.pos, p2.pos) < cutoff) {
+                ctx.moveTo(p.pos.x, p.pos.y);
+                ctx.lineTo(p2.pos.x, p2.pos.y);
+                ctx.stroke();
+            }
+        }
+
+        p.show();
+    }
+
+    requestAnimationFrame(ani);
 }
 
 init();
